fe02c7477be0e7c23810be1672c21b8deaf9d47e
bagas557tns <bagas557@newschool.edu>
2022-03-25 09:54:36 -0400

    Java and CSS Changes to Shenandoah

--- ci2/activities/js-lab1/script.js
+++ ci2/activities/js-lab1/script.js
@@ -1,12 +1,9 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
-
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -52,3 +49,64 @@ window.onscroll = function() {scrollFunction()};
 function topFunction() {
   document.documentElement.scrollTop = 0;
 }
+
+
+
+
+var anythingIWant = document.body;
+// Setting variable (let x =...)
+
+var isDark = false;
+
+// If button is clicked:
+var darkModeButton = document.getElementById("button3");
+darkModeButton.addEventListener("click", changeToDarkMode);
+
+// Changing color and background
+function changeToDarkMode() {
+  if (isDark === true) {
+    
+    darkModeButton.textContent = "Dark Mode";
+    anythingIWant.style.background = "white";
+    anythingIWant.style.color = "black";
+    isDark = false;
+    console.log("dark mode is off")
+    
+  } else if (isDark === false) {
+    
+    darkModeButton.textContent = "Light Mode";
+    anythingIWant.style.background = "black";
+    anythingIWant.style.color = "white";
+    isDark = true;
+    console.log("dark mode is on!")
+  }
+}
+
+
+var textButton = document.getElementById("button4");
+textButton.addEventListener("click", changeToSans);
+var text = document.getElementById("info");
+
+function changeToSans() {
+  text.style.fontSize = "3.5em"
+}
+
+// var isSerif = false
+
+  // if (isSerif === true) {
+      // text.style.fontFamily = "sans-serif"
+
+// }  else (isSerif === false) {
+  // text.style.fontFamily = "serif"
+// }
+// }
+
+
+
+
+
+
+
+
+
+
